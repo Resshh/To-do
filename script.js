c99d3c2fb770b37fcee5f9799097a0e6d8f1a54f
Reshma Ann Thomas <reshmaannthomas10@gmail.com>
2026-07-13 13:13:22 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function loginForm() {
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+
+    if (username == "admin" & password == 12345) {
+
+    }
+    else {
+        alert("incorrect")
+    }
+}
